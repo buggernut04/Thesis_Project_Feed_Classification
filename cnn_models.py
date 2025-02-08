8428d8f49269428d95e0b93094d00a08e24d861e
buggernut04 <cs_root@Room102IC-08.localdomain>
2025-02-08 12:45:53 +0800

    modified program

--- cnn_models.py
+++ cnn_models.py
@@ -8,7 +8,7 @@ def CustomModel():
     custom_model = Sequential()
 
     # First Layer
-    custom_model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(400, 400, 3)))
+    custom_model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(400, 400, 3)))
     custom_model.add(layers.BatchNormalization())  # Add Batch Normalization
     custom_model.add(layers.MaxPooling2D((2, 2)))
 
@@ -18,7 +18,7 @@ def CustomModel():
     custom_model.add(layers.MaxPooling2D((2, 2)))
 
     # Third Layer
-    custom_model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    custom_model.add(layers.Conv2D(128, (3, 3), activation='relu'))
     custom_model.add(layers.BatchNormalization())  # Add Batch Normalization
     custom_model.add(layers.MaxPooling2D((2, 2)))
 
@@ -26,7 +26,7 @@ def CustomModel():
     custom_model.add(layers.Flatten())
     custom_model.add(layers.Dropout(0.5))  # Add Dropout
 
-    custom_model.add(layers.Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.001)))  # L2 Regularization
+    custom_model.add(layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.001)))  # L2 Regularization
     custom_model.add(layers.Dropout(0.5))  # Add Dropout
 
     custom_model.add(layers.Dense(1, activation='sigmoid'))
@@ -49,8 +49,9 @@ def ResNetModel():
 
     # Add the layers
     resnet_model.add(pretrained_model)
-    resnet_model.add(layers.Dense(128, activation = 'relu'))
-    resnet_model.add(layers.Dropout(0.5))
+    resnet_model.add(layers.Dense(32, activation='relu', kernel_regularizer=regularizers.l2(0.001)))  # L2 Regularization
+    resnet_model.add(layers.Dropout(0.3))
+    resnet_model.add(layers.BatchNormalization())
 
     # the last layer must specify how many number classes needed to evaluate
     resnet_model.add(layers.Dense(1, activation = 'sigmoid'))
